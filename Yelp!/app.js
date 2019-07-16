6b1929c033e03baa3391c516fc6a0f820a373498
Anh Pham <anh65498@gmail.com>
2019-07-15 20:23:41 -0700

    Add mongoose database to store destinations and add new destination to mongoose Db

--- Yelp!/app.js
+++ Yelp!/app.js
@@ -1,8 +1,34 @@
 /* Main backend for our Yelp app */
 const port        = 5000
-var express       = require("express")
-var bodyParser    = require("body-parser")    // for Express's post method
+var express       = require("express"),
+    bodyParser    = require("body-parser"),    // for Express's post method
+    mongoose      = require("mongoose")
+
 var app           = express()
+// tell Express to look inside "public" directory for CSS files
+app.use(express.static("public"))
+// tell Express to use body parser to parse client's request's information (like form's input)
+app.use(bodyParser.urlencoded({extended:true}));
+ // create Db inside mongoDB or use existing Db
+mongoose.connect("mongodb://localhost/YelpTravel_destinations", { useNewUrlParser: true })
+// DB Schema setup - define data types of DB
+var destinationSchema = new mongoose.Schema({
+  name: String,
+  state: String,
+  country: String,
+  image: String
+})
+var Destination = mongoose.model("Destination", destinationSchema)    // create a new collection called destination in YelpTravel_destinations database
+
+// Destination.create({
+//   name: "Emerald Bay State Park", state: "California", image: "photos/emerald_bay.jpg", country: "USA"
+// }, function(err, dest) {
+//   if (err)
+//     console.log("Error adding new destination to Database: " + err)
+//   else
+//     console.log("New destination was added to Database: " + dest)
+// })
+
 var destinations = [
       { name: "Emerald Bay State Park", state: "California", img: "photos/emerald_bay.jpg"},
       { name: "Disneyland", state:"California", img: "photos/disneyland.jpg"},
@@ -15,17 +41,19 @@ var destinations = [
       { name: "Disneyland", state:"California", img: "photos/disneyland.jpg"},
       { name: "Santa Monica Beach", state:"California", img: "photos/santa_monica.jpg"}]   // array of objects
 
-// tell Express to look inside "public" directory for CSS files
-app.use(express.static("public"))
-// tell Express to use body parser to parse client's request's information (like form's input)
-app.use(bodyParser.urlencoded({extended:true}));
 // HOMEPAGE
 app.get("/", (req, res) => {
   res.render("landing.ejs")
 })
 
 app.get("/destinations", (req, res) => {
-  res.render("destinations.ejs", {destinations : destinations})
+  // Get all campgrounds from Db
+  Destination.find({}, (error, results) => {
+    if (error)
+      console.log("Error when retrieving destinations from database: " + error)
+    else
+      res.render("destinations.ejs", {destinations : results})
+  })
 })
 
 // Creating new destinations, same URL as 'get' method cuz REST
@@ -36,10 +64,17 @@ app.post("/destinations", (req, res) => {
   var newDest = {
           name: req.body.destName,
           state: req.body.state,
-          img: req.body.img_url}
-  destinations.push(newDest)
-  // redirect back to /destinations page of 'get' request
-  res.redirect("/destinations")
+          country: req.body.country,
+          image: req.body.img_url}
+  Destination.create(newDest, (error, newlyCreated) => {
+    if (error)
+      console.log("Error when updating database: " + error)
+    else{
+      // redirect back to /destinations page of 'get' request
+      res.redirect("/destinations")
+    }
+  })
+
 })
 
 // Show Form to add new destination to database
