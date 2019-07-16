6ede28fac8632425006376bde0d1ccd05d3b6770
Anh Pham <anh65498@gmail.com>
2019-07-16 13:40:32 -0700

    Setup app.js, add INDEX route and index template, add landing page

---
+++ Journal to Success/app.js
@@ -0,0 +1,51 @@
+const port        = 5000
+var express       = require("express"),
+    app           = express(),
+    bodyParser    = require("body-parser"),
+    mongoose      = require("mongoose")
+
+// tell Express to look inside "public" directory for CSS files
+app.use(express.static("public"))
+// tell Express to use body parser to parse client's request's information (like form's input)
+app.use(bodyParser.urlencoded({extended:true}));
+// create Db inside mongoDB or use existing Db
+mongoose.connect("mongodb://localhost/Journal_to_Success", { useNewUrlParser: true })
+// DB Schema setup - define data types of DB
+var journalSchema = new mongoose.Schema({
+  dateCreated: {type: Date, default: Date.now},
+  completedTasks: String,    // 3-5 items so you dont feel overwhelm
+  event: String,
+  note: String,
+  learn: String,
+  emotion: String,
+  celebration: String,  //  write down the 3 major wins of the day, every single day.
+  goalTomorrow: String,   // self, work, relationship,
+  habit: String,
+  habitStreak: Number,
+})
+var Journal = mongoose.model("Journal", journalSchema)  // create a new collection called journals in Journal_to_Success database
+
+// Journal.create({
+//   title: "Test",
+//   todosToday: "Study Udemy",    // 3-5 items so you dont feel overwhelm
+//   learn: "mongoose, REST",
+// })
+
+// RESTful Routes
+// Landing page
+app.get("/", (req, res) => {
+  res.render("landing-page.ejs")
+})
+
+// Index
+app.get("/journals", (req, res) => {
+  Journal.find({}, (error, retJournals) =>{   // return journals from Db
+    if (error)
+      console.log("Error opening the index page: " + error)
+    else
+      res.render("index.ejs", {journals : retJournals})
+  })
+})
+
+
+app.listen(port, () => console.log(`Journal App server is listening on port ${port}`))
