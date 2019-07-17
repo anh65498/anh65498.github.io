4fe64d8aad08271e11861c8e91b1f040659af237
Anh Pham <anh65498@gmail.com>
2019-07-17 15:07:17 -0700

    Add Delete Route, Edit and Delete button in Show page

--- Journal to Success/app.js
+++ Journal to Success/app.js
@@ -1,13 +1,16 @@
 const port        = 5000
 var express       = require("express"),
     app           = express(),
-    bodyParser    = require("body-parser"),
-    mongoose      = require("mongoose")
+    bodyParser    = require("body-parser"),     // to parse client's form to JS object
+    mongoose      = require("mongoose"),         // for database
+    methodOverride = require("method-override")
 
 // tell Express to look inside "public" directory for CSS files
 app.use(express.static("public"))
 // tell Express to use body parser to parse client's request's information (like form's input)
 app.use(bodyParser.urlencoded({extended:true}));
+// tell Express to use method-override to
+app.use(methodOverride("_method"))
 // create Db inside mongoDB or use existing Db
 mongoose.connect("mongodb://localhost/Journal_to_Success", { useNewUrlParser: true })
 // DB Schema setup - define data types of DB
@@ -24,6 +27,9 @@ var journalSchema = new mongoose.Schema({
   habitStreak: Number,
 })
 var Journal = mongoose.model("Journal", journalSchema)  // create a new collection called journals in Journal_to_Success database
+// Make Mongoose use `findOneAndUpdate()`. Note that this option is `true`
+// by default, you need to set it to false.
+mongoose.set('useFindAndModify', false);
 
 // Journal.create({
 //   title: "Test",
@@ -90,7 +96,29 @@ app.get("/journals/:id/edit", (req, res) =>{
 })
 
 // Update Route: When user click "submit" button in Edit page, this route is activated
+// install method-override because with PUT method, add user's data is shown in URL.
+// Use method-override package to convert POST method from the form to PUT to hide user's data
 app.put("/journals/:id", (req, res) =>{
-  Journal.findByIdAndUpdate()
+  Journal.findByIdAndUpdate(req.params.id, req.body.journal, (error, updatedJournal) =>{
+    // params: id, newData from URL (masked by POST method in form but actually it shows in PUT method), callback function
+    if (error)
+      res.redirect("/journals")
+    else
+      res.redirect("/journals/" + req.params.id)
+  })
+})
+
+// DELETE route: delete a journal
+app.delete("/journals/:id", (req, res) =>{
+  // destroy the journal
+  Journal.findByIdAndRemove(req.params.id, (error) =>{
+    if (error){
+      console.log("Error deleting post: " + error)
+      res.redirect("/journals")
+    }
+    else
+      res.redirect("/journals")
+  })
+
 })
 app.listen(port, () => console.log(`Journal App server is listening on port ${port}`))
