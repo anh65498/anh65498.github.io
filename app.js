57df44344a3a143e66850d4e0f8099eabe5bf726
Anh Pham <anh65498@gmail.com>
2019-07-20 13:14:27 -0700

    Add route to show forms to create new a project, event, and mentor

--- app.js
+++ app.js
@@ -5,10 +5,51 @@ var express         = require("express"),
 
 
 
-// LANDING PAGE  - Done (show )
+
+
+// ===============
+// PUBLIC ROUTES
+// ===============
+// Landing page - Welcoming page (Done)
 app.get("/", (req, res) => {
   res.render("landing.ejs")
 })
+
+// Event page - Show a list of events
+app.get("/events", (req, res) => {
+  res.render("events.ejs")
+})
+
+// Mentor page - Show mentors (Hall of Fame)
+app.get("/mentors", (req, res) => {
+  res.render ("mentors.ejs")
+})
+
+// Project page - Show projects
+app.get("/projects", (req, res) => {
+  res.render("projects.ejs")
+})
+
+
+
+// ==========================================
+// USER ROUTES (only user can access these)
+// ==========================================
+// SHOW FORMS: to create new projects, events, women
+app.get("/projects/new", (req, res) =>{
+  res.render("new_project.ejs")
+})
+
+app.get("/events/new", (req, res) =>{
+  res.render("new_event.ejs")
+})
+
+app.get("/mentors/new", (req, res) => {
+  res.render("new_mentor.ejs")
+})
+
+// CREATE: take input from users and
+
 
 
 app.listen(port, () => console.log(`Seeds Server is listening on port ${port}`))
