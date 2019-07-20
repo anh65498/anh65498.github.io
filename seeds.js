fd92d8179150896155d68d4d92b1b1a4329a496a
Ran He <ranh@usc.edu>
2019-07-20 16:39:09 -0700

    Add fake data to seeds.js

---
+++ seeds.js
@@ -0,0 +1,146 @@
+// run this to empty current database and "seed" database with some users
+
+var mongoose = require("mongoose");
+// var Destination = require("./MongoDB_models/destination.js"),
+//     Comment   = require("./MongoDB_models/comment");
+
+var mentorData = [
+  {
+    // "_id" : ObjectId("5d32b4d0fd13e359f9d0c594"),
+    "name" : "Gwen",
+    "skills" : "Business Strategy & Planning, Product Management",
+    "experience" : "Product Manager",
+    "projects" : "Business strategy planning meeting, improve the features on current projects",
+    "company" : "Twilio",
+    "cover_img" : "./photos/gwen.jpg"
+  },
+  {
+    // "_id" : ObjectId("5d32b46cfd13e359f9d0c590"),
+    "name" : "Grace",
+    "skills" : "Java, Python, HTML, CSS",
+    "experience" : "Software Engineer",
+    "projects" : "Various projects in full-stack, front-end, and back-end",
+    "company" : "Venmo",
+    "cover_img" : "./photos/grace.jpg"
+  },
+  {
+    // "_id" : ObjectId("5d32b4a2fd13e359f9d0c592"),
+    "name" : "Lucy",
+    "skills" : "Data Mining, Machine Learning",
+    "experience" : "Applied Data Scientist",
+    "projects" : "Research projects in deep learning and computer vision",
+    "company" : "Amazon",
+    "cover_img" : "./photos/lucy.jpg"
+  },
+  {
+    // "_id" : ObjectId("5d32b4e7fd13e359f9d0c595"),
+    "name" : "Vivian",
+    "skills" : "React, Angular",
+    "experience" : "Software Engineer",
+    "projects" : "Large scale web applications",
+    "company" : "HERE",
+    "cover_img" : "./photos/vivian.jpg"
+  }
+]
+
+var eventData = [
+  {
+    // "_id" : ObjectId("5d32b4d0fd13e359f9d0c594"),
+    "name" : "Spectra Hackathon",
+    "date" : "2019-07-20",
+    "time" : "08:30:00",
+    "location" : "Make School, 555 Post St, San Francisco, CA 94102"
+  },
+  {
+    // "_id" : ObjectId("5d32b46cfd13e359f9d0c590"),
+    "name" : "Twilio Customer & Developer Conference",
+    "date" : "2019-08-06",
+    "time" : "09:00:00",
+    "location" : "Moscone West, 800 Howard Street, San Francisco, CA 94107"
+  },
+  {
+    // "_id" : ObjectId("5d32b4a2fd13e359f9d0c592"),
+    "name" : "GopherCon",
+    "date" : "2019-07-24",
+    "time" : "07:30:00",
+    "location" : "Marriott Marquis San Diego Marina, California"
+  },
+  {
+    // "_id" : ObjectId("5d32b4e7fd13e359f9d0c595"),
+    "name" : "Now at Work global event series",
+    "date" : "2019-10-08",
+    "time" : "08:00:00",
+    "location" : "Beanfield Centre, Toronto, Canada"
+  }
+]
+
+var projectData = [
+  {
+    // "_id" : ObjectId("5d32b4d0fd13e359f9d0c594"),
+    "name" : "Data Analysis and Machine Learning Projects",
+    "description" : "Collection of teaching materials, code, and data for my data analysis and machine learning projects.",
+    "tools" : "Python, Jupyter Notebook",
+    "cover-img" : "./photos/ml.jpg",
+    "author" : "Jenny"
+  },
+  {
+    // "_id" : ObjectId("5d32b46cfd13e359f9d0c590"),
+    "name" : "React Projects",
+    "description" : "Web applications and websites designed with React",
+    "tools" : "React",
+    "cover-img" : "./photos/react.jpg",
+    "author" : "Nicole"
+  },
+  {
+    // "_id" : ObjectId("5d32b4a2fd13e359f9d0c592"),
+    "name" : "AI Projects",
+    "description" : "Artificial Intelligence projects, documentation and code",
+    "tools" : "Python",
+    "cover-img" : "./photos/ai.jpg",
+    "author" : "Will"
+  },
+  {
+    // "_id" : ObjectId("5d32b4e7fd13e359f9d0c595"),
+    "name" : "iOS Projects",
+    "description" : "Class projects about iOS development and interface design",
+    "tools" : "Swift",
+    "cover-img" : "./photos/swift.jpg",
+    "author" : "Annie"
+  }
+]
+
+function seedDB(){
+  // Empty current database
+  Mentor.deleteMany({}, (error) => {
+    if (error)
+      console.log("Error when clearing file in seeds.js: " + error)
+    else {
+      console.log("Database is cleared succesfully")
+      // Add some destinations to YelpTravel_destinations database from data list above
+      for (var mentor of mentorData){
+        Mentor.create(mentor, (error, retDest) => {
+            if (error) console.log(error)
+            else{
+              console.log("Added a Mentor.")
+              // Create a comment
+              // Comment.create({
+              //   content: "This place is great, but I wish there was Internet.",
+              //   author: {
+              //     id: "5d32b4b2fd13e359f9d0c593",
+              //     username: "zach"
+              //   }
+              // }, (error, retComment)=>{
+              //   if (error) console.log("Error creating new comment in seeds.js: " + error)
+              //   else {
+              //     retDest.comments.push(retComment)
+              //     retDest.save()
+              //     console.log("Create 1 new comment!")
+              //   }
+              // })
+            }
+          })
+        }
+    }
+  })
+
+module.exports = seedDB;  // return this function to whatever variable calls require("seeds.js")
